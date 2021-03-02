4fce84e2209bbe8d19fd2dff85b95c1a31848597
ty <tylerpmatthews1998@gmail.com>
2021-03-02 16:09:09 -0500

    heroku push

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,14 +1,28 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('user_username', 200).notNullable()
-      users.string('user_password', 200).notNullable()
-      users.string('user_email', 320).notNullable()
-      users.timestamps(false, true)
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("username", 200).notNullable().unique();
+      users.string("password", 200).notNullable();
+      users.timestamps(false, true);
     })
-}
+    .createTable("recipes", (tbl) => {
+      tbl.increments("recipe_id");
+      tbl.string("title", 128).notNullable().unique();
+      tbl.string("recipe_source", 128).notNullable();
+      tbl.string("recipe_image", 128);
+      tbl.text("ingredients").notNullable();
+      tbl.string("category", 128).notNullable();
+      tbl
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("RESTRICT");
+    });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema.dropTableIfExists("users").dropTableIfExists("recipes");
+};
