278c1bc874e80162fe47bec1f52528511b58de0f
ty <tylerpmatthews1998@gmail.com>
2021-03-04 16:43:20 -0500

    replaced * with / inside index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000
 
 server.use(express.static(path.join(__dirname, 'client/dist')))
 
-server.get('*', (req, res) => {
+server.get('/', (req, res) => {
   // if you want to serve a SPA using Express you totally can!!
   res.sendFile(path.join(__dirname, 'client/dist', 'index.html'))
 })
